f380edf9aab883dba18f936ef6e4d96f8b24064f
admin <koratutkarsh310@gmail.com>
2022-12-30 10:33:16 +0530

    class3---30/12/2022

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Text(
+              "Hello\n\n\n\n\n\n\n\n\nDart\n\n\n\n\n\n\n\n\nFlutter",
+              style: TextStyle(fontSize: 30,color: Colors.red.shade700,decoration: TextDecoration.underline,decorationColor: Colors.yellow,fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    ),
+  );
+}
